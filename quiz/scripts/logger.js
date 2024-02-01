7ed4e1e38bed11ffe57debd785e1242f3db8a58b
nishyanntomy <nishyanntomy@gmail.com>
2024-02-01 15:04:02 -0500

    Update changes from class on date 01/30/2024

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,11 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg() {
+    console.log(this.errMsg);
+}
+
+
+const e1 = {
+    errMsg: "Oops!"
+}
+
+logMsg.call(e1);
